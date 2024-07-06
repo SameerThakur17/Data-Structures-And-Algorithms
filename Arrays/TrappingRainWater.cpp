f5344afac369700d89660558d001f6be0eaaaada
Sameer Thakur <samthakur1742002@gmail.com>
2024-07-06 15:27:44 +0530

    Correction

--- Arrays/TrappingRainWater.cpp
+++ Arrays/TrappingRainWater.cpp
@@ -37,22 +37,23 @@ int trappingRainWaterOptimized(int arr[], int n)
     {
         lmax[i] = max(lmax[i - 1], arr[i]);
     }
-    rmax[0] = arr[n - 1];
+    rmax[n - 1] = arr[n - 1];
     for (int i = n - 2; i >= 0; i--)
     {
         rmax[i] = max(rmax[i + 1], arr[i]);
     }
+
     int res = 0;
     for (int i = 1; i < n - 1; i++)
     {
-        res = min(lmax[i], rmax[i]) - arr[i];
+        res += min(lmax[i], rmax[i]) - arr[i];
     }
     return res;
 }
 
 int main()
 {
-    int arr[3] = {2, 0, 2};
-    cout << trappingRainWater(arr, 3);
+    int arr[6] = {4, 2, 0, 3, 2, 5};
+    cout << trappingRainWater(arr, 6);
     return 0;
 }
