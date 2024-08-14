e5fed4f9b0a93b4385c604d98cb38f2967d09ca4
Sameer Thakur <samthakur1742002@gmail.com>
2024-08-14 10:45:28 +0530

    Sorting Algos

--- Sorting/SelectionSort.cpp
+++ Sorting/SelectionSort.cpp
@@ -31,6 +31,28 @@ void selectionSortNaive(int arr[], int n)
 
 // Selection Sort Optimized
 
+void selectionSortOptimized(int arr[], int n)
+{
+    /*
+    Time Complexity O(n^2)
+    Space Complexity O(1)
+    Unstable Algorithm
+    In-place Algorithm
+    */
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
 int main()
 {
     int arr[6] = {10, 5, 6, 3, 8, 11};
@@ -40,7 +62,8 @@ int main()
         cout << arr[i] << " ";
     }
     cout << endl;
-    selectionSortNaive(arr, 6);
+    // selectionSortNaive(arr, 6);
+    selectionSortOptimized(arr, 6);
     cout << "Sorted array " << endl;
     for (int i = 0; i < 6; i++)
     {
